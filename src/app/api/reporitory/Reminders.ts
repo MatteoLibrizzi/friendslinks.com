8f5994e4cbce847693ce7e4c9c56aa2863d5bc90
Matteo Librizzi <mttlbr@amazon.lu>
2024-10-18 12:01:56 -0700

    Added contacts list

--- src/app/api/reporitory/Reminders.ts
+++ src/app/api/reporitory/Reminders.ts
@@ -13,19 +13,28 @@ interface Reminder {
 export abstract class RemindersRepository {
     abstract addReminder: (reminder: Reminder) => Promise<void>;
     abstract getRemindersByContactInfo: (contactInfo: string) => Promise<Reminder[]>;
+    abstract getAllEmails: () => Promise<string[]>;
 }
 
 export class KVRemindersRepository extends RemindersRepository {
     addReminder = async (reminder: Reminder): Promise<void> => {
         await kv.lpush(`reminders:${reminder.contactInfo}`, reminder)
+        await kv.lpush(`reminders:contacts`, reminder.contactInfo)
 
         console.log(`Added reminder to reminders:${reminder.contactInfo}, new list length: `, await kv.llen(`reminders:${reminder.contactInfo}`))
     }
-// TODO clear db and add with new schema
+    // TODO clear db and add with new schema
     getRemindersByContactInfo = async (contactInfo: string): Promise<Reminder[]> => {
         const numberOfReminders = await kv.llen(`reminders:${contactInfo}`)
         const reminders = await kv.lrange(`reminders:${contactInfo}`, 0, numberOfReminders - 1) as any[]
 
         return reminders ?? []
+    }
+
+    getAllEmails: () => Promise<string[]> = async () => {
+        const numberOfEmails = await kv.llen(`reminders:contacts`)
+        const emails = await kv.lrange(`reminders:contacts`, 0, numberOfEmails - 1) as any[]
+
+        return emails ?? []
     }
 }
